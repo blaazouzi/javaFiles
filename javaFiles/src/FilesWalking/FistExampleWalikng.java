39e64baad37873b2ed1d4de537d385881189a4ee
Brahim Laazouzi <laaz.braid@gmail.com>
2021-07-27 15:25:13 -0400

    second commit

--- javaFiles/src/FilesWalking/FistExampleWalikng.java
+++ javaFiles/src/FilesWalking/FistExampleWalikng.java
@@ -1,10 +1,27 @@
 package FilesWalking;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class FistExampleWalikng {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Path path =Paths.get("c:/temp");
+		try {
+			long count=Files.walk(path)
+			      .filter(p->p.toString().endsWith(".java"))
+			      .count();
+			System.out.println("count file="+count);
+		}
+		
+		catch(IOException e) {}
+		
+
+		}
 
 	}
 
-}
+
+
